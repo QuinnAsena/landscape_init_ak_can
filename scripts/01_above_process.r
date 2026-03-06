4183bca67af13317c7f8d52d3ce2ecbc12c2764d
QuinnAsena <asenaq@caryinstitute.org>
2026-03-06 16:11:01 -0500

    re-written to use Lora's original polygons

--- scripts/01_above_process.r
+++ scripts/01_above_process.r
@@ -16,12 +16,31 @@ above_water_files <- list.files(
 # Filter out Quality Assurance
 above_water_files <- above_water_files[!grepl(pattern = "QA", above_water_files)]
 
-# We are using the plot areas even though they already are the above lc data
-landscapes_files <- list.files(
-  path = "//10.60.2.10/FF_Lab/personal_storage/quinn_storage/ak_plot_areas",
-  pattern = "\\.tif$", full.names = TRUE)
+# Load up Lora's selected landscapes
+landscapes_poly <- vect("//10.60.2.10/FF_Lab/project_data/na_boreal/Landscape building/Landscape selection/Selected plots/final_plots.shp")
 
-landscapes_files <- landscapes_files[order(as.integer(sub(".*?(\\d+)\\.tif$", "\\1", landscapes_files)))]
+
+# Awkwardly stich cpcrw on
+cpcrw <- rast("D:/quinn/GitHub/landscape_init_ak_can/data/cpcrw/env.grid.tif")
+cpcrw_poly <- as.polygons(cpcrw, extent = TRUE) |>
+  project(crs(landscapes_poly))
+plot(cpcrw_poly)
+same.crs(cpcrw_poly, landscapes_poly)
+
+cpcrw_poly_proj$ID            <- 1
+cpcrw_poly_proj$ecoregion     <- "ALASKA CPCRW"
+cpcrw_poly_proj$Propforest     <- NA
+cpcrw_poly_proj$Suppressio     <- NA
+
+landscapes_poly <- rbind(cpcrw_poly_proj, landscapes_poly)
+
+
+landscapes_poly$landscape_id <- ifelse(
+  grepl("ALASKA", landscapes_poly$ecoregion),
+  paste0("alaska_landscape_", sprintf("%02d", seq_len(nrow(landscapes_poly)))),
+  paste0("canada_landscape_", sprintf("%02d", seq_len(nrow(landscapes_poly))))
+)
+
 
 # Create vrt once outside function
 above_lc_vrt <- vrt(above_lc_files, set_names = TRUE)
@@ -42,6 +61,40 @@ water_vrt_decade <- lapply(above_water_decade, \(r) {
 water_vrt <- Reduce(c, water_vrt_decade)
 
 
+
+
+crop_lcp <- function(lcp_vrt, poly) {
+  lcp <- paste0("landscape_", poly$landscape_id)
+  nm <- sub("_\\d+$", "", names(lcp_vrt)[1])
+  out <- file.path(here(), lcp, nm)
+  dir.create(out, recursive = TRUE)
+
+  if (!same.crs(lcp_vrt, poly)) {
+    warning("Check on crs in: ", lcp, "\nprojecting to ", crs(lcp_vrt))
+    poly <- project(poly, crs(lcp_vrt))
+  }
+
+  vrt_crop <- suppressWarnings(crop(lcp_vrt, poly, mask = TRUE))
+
+  writeRaster(vrt_crop, file.path(out, paste0(nm, ".tif")), overwrite = TRUE, datatype = "INT4S")
+}
+
+
+landscapes <- lapply(
+  seq_len(nrow(landscapes_poly)),
+  \(i) crop_lcp(above_lc_vrt, landscapes_poly[i, ])
+)
+
+landscapes <- lapply(
+  seq_len(nrow(landscapes_poly)),
+  \(i) crop_lcp(water_vrt, landscapes_poly[i, ])
+)
+
+
+
+
+
+
 # Now we crop all 5 landscapes out of the vrt and save the rasters
 vrt_lcp <- function(lcp_vrt, landscapes_file) {
   nm <- sub("_\\d+$", "", names(lcp_vrt)[1])
@@ -53,9 +106,9 @@ vrt_lcp <- function(lcp_vrt, landscapes_file) {
 
   if (!same.crs(lcp_crop_area, lcp_vrt)) {
     stop("Check on crs")
-  } 
-  vrt_crop <- crop(lcp_vrt, lcp_crop_area)
-  
+  }
+  vrt_crop <- crop(lcp_vrt, lcp_crop_area, mask = TRUE)
+
   writeRaster(vrt_crop, file.path(out, paste0(nm, ".tif")), overwrite = TRUE, datatype = "INT4S")
 }
 
@@ -63,8 +116,95 @@ lapply(landscapes_files, vrt_lcp, lcp_vrt = above_lc_vrt)
 lapply(landscapes_files, vrt_lcp, lcp_vrt = water_vrt)
 
 tst <- rast("D:/quinn/GitHub/landscape_init_ak_can/landscape_06/ABoVE_LandCover/ABoVE_LandCover.tif")
-plot(tst)
+plot(tst[[1]])
 tst1 <- ifel(is.na(tst), 1, NA)
 plot(tst1)
 tst2 <- rast("D:/quinn/GitHub/landscape_init_ak_can/landscape_01/ABoVE_Water/ABoVE_Water.tif")
 plot(tst2)
+
+
+landscapes_poly <- vect("//10.60.2.10/FF_Lab/project_data/na_boreal/Landscape building/Landscape selection/Selected plots/final_plots.shp")
+
+landscapes <- lapply(seq_len(nrow(landscapes_poly)), \(i) {
+  lcp_crop <- crop(above_lc_vrt, landscapes_poly[i, ], mask = TRUE)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# We are using the plot areas even though they already are the above lc data
+landscapes_poly <- vect("Z:/project_data/na_boreal/Landscape building/Landscape selection/Selected plots/final_plots.shp")
+plot(landscapes_poly)
+r <- rast(above_lc_files[4])
+same.crs(r, landscapes_poly)
+
+above_tst_crop <- crop(above_lc_vrt, landscapes_poly[1, ], mask = TRUE)
+plot(above_tst_crop[[1]])
+tst <- rast("D:/quinn/GitHub/landscape_init_ak_can/landscape_01/ABoVE_LandCover/ABoVE_LandCover.tif")
+plot(tst)
+compareGeom(above_tst_crop, tst)
+all.equal(above_tst_crop, tst)
+
+above_tst_crop2 <- crop(above_lc_vrt, landscapes_poly[1, ], mask = TRUE)
+plot(above_tst_crop2)
+
+cpcrw_ru <- rast("D:/quinn/GitHub/iLand_automated/gis/env.grid.tif")
+plot(cpcrw_ru)
+
+cpcrw <- rast("Z:/personal_storage/quinn_storage/ak_plot_areas/env.grid_cpcrw06.tif")
+plot(cpcrw)
+
+
+
+
+
+
+
+# We are using the plot areas even though they already are the above lc data
+cpcrw_landscapes_files <- list.files(
+  path = "//10.60.2.10/FF_Lab/personal_storage/quinn_storage/ak_plot_areas",
+  pattern = "\\.tif$", full.names = TRUE)
+
+cpcrw_landscapes_files <- cpcrw_landscapes_files[!grepl(pattern = "landscapes", cpcrw_landscapes_files)]
+
+cpcrw_names <- basename(cpcrw_landscapes_files)
+
+cpcrw_rasts <- lapply(cpcrw_landscapes_files, rast)
+names(cpcrw_rasts) <- cpcrw_names
+
+plot(cpcrw_rasts$env.grid.tif)
+plot(cpcrw_rasts$env.grid_cpcrw06.tif)
+plot(cpcrw_rasts$forest_species_init.tif)
+plot(cpcrw_rasts$master_gridf.tif)
+plot(cpcrw_rasts$stand_grid.tif)
+
+
+env.grid_proj <- project(cpcrw_rasts$env.grid.tif, crs(cpcrw_rasts$env.grid_cpcrw06.tif), method = "near")
+plot(env.grid_proj)
+
+
+env.grid_proj06 <- project(cpcrw_rasts$env.grid_cpcrw06.tif, crs(cpcrw_rasts$env.grid.tif), method = "near")
+plot(env.grid_proj06)
+
+
+
+above_tst_crop_proj <- project(above_tst_crop, crs(cpcrw_rasts$env.grid.tif), method = "near")
+plot(above_tst_crop_proj)
