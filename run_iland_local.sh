0d09824a354d86b83e87c90aed265cac734e3ad3
QuinnAsena <asenaq@caryinstitute.org>
2026-04-30 11:22:04 -0400

    updating path

---
+++ run_iland_local.sh
@@ -0,0 +1,10 @@
+ILANDC_BIN="D:/quinn/iLand2.1/ilandc" \
+ILANDC_OUTPUT_ROOT="landscape_alaska_02/output" \
+bash run_iland_csv_cpxml.sh \
+    landscape_alaska_02/landscape_alaska_02_1950-1980spinup_test.xml 1 1 5
+
+
+ILANDC_BIN="Z:/personal_storage/quinn_storage/landscape_init_ak_can/iLandc_container/ilandv2p1.sif" \
+ILANDC_OUTPUT_ROOT="landscape_alaska_01/output" \
+bash run_iland_csv_cpxml.sh \
+    landscape_alaska_01/landscape_alaska_01_1950-1980spinup_test.xml 1 1 5
--- scripts/run_iland_local.sh
+++
@@ -1,4 +0,0 @@
-ILANDC_BIN="D:/quinn/iLand2.1/ilandc" \
-ILANDC_OUTPUT_ROOT="landscape_alaska_02/output" \
-bash run_iland_csv_cpxml.sh \
-    landscape_alaska_02/landscape_alaska_02_1950-1980spinup_test.xml 1 1 5
