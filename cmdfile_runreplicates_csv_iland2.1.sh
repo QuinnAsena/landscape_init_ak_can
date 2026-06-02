9c0d3b2537795120df7896e444c239be8fc86cd8
QuinnAsena <asenaq@caryinstitute.org>
2026-06-02 09:44:27 -0400

    landscape 2 reps 7-12

--- cmdfile_runreplicates_csv_iland2.1.sh
+++ cmdfile_runreplicates_csv_iland2.1.sh
@@ -6,18 +6,18 @@
 # bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_01/landscape_alaska_01_1950-1980spinup.xml 7 7 300
 # bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_01/landscape_alaska_01_1950-1980spinup.xml 8 8 300
 # bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_01/landscape_alaska_01_1950-1980spinup.xml 9 9 300
-bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_01/landscape_alaska_01_1950-1980spinup.xml 10 10 300
-bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_01/landscape_alaska_01_1950-1980spinup.xml 11 11 300
-bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_01/landscape_alaska_01_1950-1980spinup.xml 12 12 300
-bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 4 4 300
-bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 5 5 300
-bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 6 6 300
-# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 7 7 300
-# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 8 8 300
-# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 9 9 300
-# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 10 10 300
-# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 11 11 300
-# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 12 12 300
+# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_01/landscape_alaska_01_1950-1980spinup.xml 10 10 300
+# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_01/landscape_alaska_01_1950-1980spinup.xml 11 11 300
+# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_01/landscape_alaska_01_1950-1980spinup.xml 12 12 300
+# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 4 4 300
+# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 5 5 300
+# bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 6 6 300
+bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 7 7 300
+bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 8 8 300
+bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 9 9 300
+bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 10 10 300
+bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 11 11 300
+bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_02/landscape_alaska_02_1950-1980spinup.xml 12 12 300
 # bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_03/landscape_alaska_03_1950-1980spinup.xml 4 4 300
 # bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_03/landscape_alaska_03_1950-1980spinup.xml 5 5 300
 # bash /glade/work/qasena/landscape_init_ak_can/run_iland_csv_cpxml_apptainer.sh /glade/work/qasena/landscape_init_ak_can/landscape_alaska_03/landscape_alaska_03_1950-1980spinup.xml 6 6 300
